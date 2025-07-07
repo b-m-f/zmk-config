a5f412d3d7033ec0c1e52448443d327c2c487628
Maximilian Ehlers <maximilian@sodawa.com>
2025-07-07 15:37:24 +0200

    fix quote->quotes and updates visuals
    Signed-off-by: Maximilian Ehlers <maximilian@sodawa.com>

--- flake.nix
+++ flake.nix
@@ -2,50 +2,56 @@
   description = "Nix flake for zmk keyboard config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { self
-    , nixpkgs
-    , flake-utils
+    {
+      self,
+      nixpkgs,
+      flake-utils,
     }:
 
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      overlays = [
-        (self: super: rec { })
-      ];
-      pkgs = import nixpkgs { inherit overlays system; };
-      zmk-viewer = pkgs.buildGoModule rec {
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [
+          (self: super: rec { })
+        ];
+        pkgs = import nixpkgs { inherit overlays system; };
+        zmk-viewer = pkgs.buildGoModule rec {
 
-        pname = "zmk-viewer";
-        version = "1.5.0";
+          pname = "zmk-viewer";
+          version = "1.5.0";
 
-        src = pkgs.fetchFromGitHub {
-          owner = "MrMarble";
-          repo = "zmk-viewer";
-          rev = "v${version}";
-          hash = "sha256-gyu0bf5XUaBWxtpoLeFIbPGqPPD2bJjEeyjfLdFy0hA=";
+          src = pkgs.fetchFromGitHub {
+            owner = "MrMarble";
+            repo = "zmk-viewer";
+            rev = "v${version}";
+            hash = "sha256-gyu0bf5XUaBWxtpoLeFIbPGqPPD2bJjEeyjfLdFy0hA=";
+          };
+
+          vendorHash = "sha256-G0p0VYCHpQE/htq452bWUZbFCxAkvwk76paiG+i72cg=";
+
+          meta = with pkgs.lib; {
+            description = "ZMK keymap visualization";
+            homepage = "https://github.com/MrMarble/zmk-viewer";
+            license = licenses.mit;
+          };
         };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            just
+            zmk-viewer
+          ];
 
-        vendorHash = "sha256-G0p0VYCHpQE/htq452bWUZbFCxAkvwk76paiG+i72cg=";
-
-        meta = with pkgs.lib; {
-          description = "ZMK keymap visualization";
-          homepage = "https://github.com/MrMarble/zmk-viewer";
-          license = licenses.mit;
+          shellHook = ''
+            echo "Ready to build firmware"
+          '';
         };
-      };
-    in
-    {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ just zmk-viewer ];
-
-        shellHook = ''
-          echo "Ready to build firmware"
-        '';
-      };
-    });
+      }
+    );
 }
